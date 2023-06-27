dda28e1f7ec6379293c228b2633a2478157a3a14
karthi <iamkarthi0412@gmail.com>
2023-06-27 15:37:45 +0530

    tstong

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'laravelmoduelscrud/laravelpermissioneditor',
+        'pretty_version' => 'dev-main2',
+        'version' => 'dev-main2',
+        'reference' => '13d08d5efede83f5864c9372dbb9768733bdc564',
+        'type' => 'metapackage',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'laravelmoduelscrud/laravelpermissioneditor' => array(
+            'pretty_version' => 'dev-main2',
+            'version' => 'dev-main2',
+            'reference' => '13d08d5efede83f5864c9372dbb9768733bdc564',
+            'type' => 'metapackage',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
